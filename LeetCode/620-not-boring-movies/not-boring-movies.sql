e9ce3a2dec2abec230a422ddc1ca72591063f853
Devraj Prasad <119845770+Devraj-prasad07@users.noreply.github.com>
2026-05-07 15:49:57 +0530

    Time: 263 ms (49.89%) | Memory: 0B (100.00%) - LeetSync

--- LeetCode/620-not-boring-movies/not-boring-movies.sql
+++ LeetCode/620-not-boring-movies/not-boring-movies.sql
@@ -1,6 +1,6 @@
--- Write your PostgreSQL query statement below
-SELECT id,movie,description,rating
-FROM Cinema 
-WHERE description != 'boring' 
-AND MOD(id,2) = 1
+# Write your MySQL query statement below
+SELECT id , movie, description, rating 
+    from Cinema 
+        WHERE id % 2 != 0  
+        AND description != 'Boring'
 ORDER BY rating DESC;
