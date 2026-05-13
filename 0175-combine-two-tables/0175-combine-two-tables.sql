a9e52675fcf8d4b784efdcb8f6242cda1dee0827
Devraj Prasad <119845770+Devraj-prasad07@users.noreply.github.com>
2026-05-13 16:11:06 +0530

    Delete 0175-combine-two-tables directory

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++
@@ -1,9 +0,0 @@
-# Write your MySQL query statement below
-SELECT firstName, lastName, city, state 
-FROM Person P
-LEFT JOIN Address A
-ON P.personId = A.personId;
-
--- Synced seamlessly with LeetHub Pro
--- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
--- Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
