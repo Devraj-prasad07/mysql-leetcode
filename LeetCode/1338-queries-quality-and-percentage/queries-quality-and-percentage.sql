71cbcd57a6d5fb6cb3dbdaea75f14ecf54cde0d4
Devraj Prasad <119845770+Devraj-prasad07@users.noreply.github.com>
2026-05-13 22:55:38 +0530

    Time: 402 ms (18.82%) | Memory: 0B (100.00%) - LeetSync

---
+++ LeetCode/1338-queries-quality-and-percentage/queries-quality-and-percentage.sql
@@ -0,0 +1,16 @@
+# Write your MySQL query statement below
+SELECT query_name,
+ROUND(AVG(rating / position),2) AS quality,
+ROUND(
+        SUM(
+            CASE 
+                WHEN rating < 3 THEN 1
+                ELSE 0
+            END
+        ) * 100 / COUNT(*),2) AS poor_query_percentage 
+FROM Queries 
+GROUP BY query_name;
+
+-- Synced seamlessly with LeetHub Pro
+-- Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+-- Get it here: https://chromewebstore.google.com/detail/leethub-v4/bcilpkkbokcopmabingnndookdogmbna
